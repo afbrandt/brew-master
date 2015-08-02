68b09c75d26988a42d64ce6449e310ee5762b4f2
Andrew Brandt <abrandt@makeschool.com>
2015-08-02 16:12:33 -0700

    project restructure

---
+++ BrewMaster.spritebuilder/Source/Bar Scene/Bartender/Bartender.swift
@@ -0,0 +1,15 @@
+//
+//  Bartender.swift
+//  BrewMaster
+//
+//  Created by Andrew Brandt on 8/2/15.
+//  Copyright (c) 2015 Dory Studios. All rights reserved.
+//
+
+import Foundation
+
+class Bartender: CCNode {
+    
+    
+    
+}
